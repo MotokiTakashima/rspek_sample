6845dbd608e5215510b8189a7eb6f46fae99b502
MotokiTakadhima <pageboy.jr@icloud.com>
2021-12-30 19:57:23 +0900

    コメントアウト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
   describe "バリデーション" do
     context "データが条件を満たすとき" do
       it "保存できる" do
