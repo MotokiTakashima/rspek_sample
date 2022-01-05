6fe292a59b1b87de9af8c16bf3658c9756158277
MotokiTakadhima <pageboy.jr@icloud.com>
2022-01-05 23:06:56 +0900

    リクエストスペックを作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  
+  describe "GET #index" do
+    it "リクエストが成功する" do
+      create_list(:user, 3)
+      get(users_path)
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
