f4804120b9d4ba29cc3a9a8c8f50d49621fd3600
MotokiTakadhima <pageboy.jr@icloud.com>
2022-01-05 23:28:09 +0900

    コードをリファクタリング

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET #index" do
-    it "リクエストが成功する" do
-      create_list(:user, 3)
-      get(users_path)
-      expect(response).to have_http_status(:ok)
-    end
-    it "name が表示されている" do
-      user1 = create(:user)
-      user2 = create(:user)
-      user3 = create(:user)
-      get(users_path)
-      expect(response.body).to include(user1.name, user2.name, user3.name)
+    subject { get(users_path) }
+    context "ユーザーが存在するとき" do
+      before { create_list(:user, 3) }
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "name が表示されている" do
+        subject
+        expect(response.body).to include(*User.pluck(:name))
+      end
     end
   end
 end
