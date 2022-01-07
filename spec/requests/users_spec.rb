13ee22f62ebd69a4541dab3e53eac0d9a1089441
MotokiTakadhima <pageboy.jr@icloud.com>
2022-01-07 22:50:20 +0900

    リクエストスペックを作成

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "GET #show" do
+    context "ユーザーが存在するとき" do
+      it "リクエストが成功する" do
+        user = create(:user)
+        get(user_path(user.id))
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "name が表示されている" do
+      end
+
+      it "age が表示されている" do
+      end
+
+      it "email が表示されている" do
+      end
+    end
+  end
 end
